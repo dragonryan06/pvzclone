ec74b1ef906b82fd24ed91c71085bc1d9f103b66
dragonryan06 <dragonryan06@gmail.com>
2024-11-21 11:16:51 -0500

    Cherry picked new canvas system from main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
-#include <vector>
-#include <memory>
 #include "FEHLCD.h"
 #include "FEHUtility.h"
 #include "global.h"
 #include "engine/input.h"
 #include "engine/interface.h"
 #include "game/game.h"
+
+#include <vector>
+#include <memory>
+#include <iostream>
 
 int main() {
     bool running = true;
@@ -19,46 +21,30 @@ int main() {
     int mostPlanted = 0;
 
     
-    // MAIN MENU
-    Sprite background(0,0,320,240,"res/backdrop/mainmenu.png");
-    TextureButton play(182,66,96,24,"res/ui/playbutton_def.png","res/ui/playbutton_prs.png");
-    TextureButton how(182,106,96,25,"res/ui/howbutton_def.png","res/ui/howbutton_prs.png");
-    TextureButton stats(203,213,40,11,"res/ui/statbutton_def.png","res/ui/statbutton_prs.png");
-    TextureButton credits(261,216,42,11,"res/ui/creditbutton_def.png","res/ui/creditbutton_prs.png");
-    
-    // Back button
-    TextureButton back(10,10,25,25,"res/ui/backbutton_def.png","res/ui/backbutton_prs.png");
-    back.hide();
-
-    // How to play menu
-    Sprite howBackground(0,0,320,240,"res/backdrop/howtoplaymenu.png");
-    howBackground.hide();
-    // Stats menu
-    Sprite statsBackground(0,0,320,240,"res/backdrop/statsmenu.png");
-    statsBackground.hide();
-    // Credits menu
-    Sprite creditsBackground(0,0,320,240,"res/backdrop/creditsmenu.png");
-    creditsBackground.hide();
-    
-
-    std::vector<CanvasItem*> mainMenuCanvas = {
-        &background,
-        &play,
-        &how,
-        &stats,
-        &credits,
-        &howBackground,
-        &statsBackground,
-        &creditsBackground,
-        &back
+    // Instantiate the main menu canvas
+    // Note: order of this vector corresponds to draw order.
+    std::vector<std::shared_ptr<CanvasItem>> mainMenuCanvas = {
+        std::shared_ptr<CanvasItem>(new Sprite(0,0,320,240,"res/backdrop/mainmenu.png")),
+        std::shared_ptr<CanvasItem>(new TextureButton(182,66,96,24,"res/ui/playbutton_def.png","res/ui/playbutton_prs.png")),
+        std::shared_ptr<CanvasItem>(new TextureButton(182,106,96,25,"res/ui/howbutton_def.png","res/ui/howbutton_prs.png")),
+        std::shared_ptr<CanvasItem>(new TextureButton(203,213,40,11,"res/ui/statbutton_def.png","res/ui/statbutton_prs.png")),
+        std::shared_ptr<CanvasItem>(new TextureButton(261,216,42,11,"res/ui/creditbutton_def.png","res/ui/creditbutton_prs.png")),
+        std::shared_ptr<CanvasItem>(new Sprite(0,0,320,240,"res/backdrop/howtoplaymenu.png")),
+        std::shared_ptr<CanvasItem>(new Sprite(0,0,320,240,"res/backdrop/statsmenu.png")),
+        std::shared_ptr<CanvasItem>(new Sprite(0,0,320,240,"res/backdrop/creditsmenu.png")),
+        std::shared_ptr<CanvasItem>(new TextureButton(10,10,25,25,"res/ui/backbutton_def.png","res/ui/backbutton_prs.png"))
     };
+    // Set menus and back button to invisible
+    mainMenuCanvas.at(5)->hide(); mainMenuCanvas.at(6)->hide();
+    mainMenuCanvas.at(7)->hide(); mainMenuCanvas.at(8)->hide();
 
-    std::vector<Button*> mainMenuClickables = {
-        &play,
-        &how,
-        &stats,
-        &credits,
-        &back
+    // Register buttons
+    std::vector<std::shared_ptr<Button>> mainMenuClickables = {
+        std::shared_ptr<Button>(static_cast<Button*>(mainMenuCanvas.at(1).get())),
+        std::shared_ptr<Button>(static_cast<Button*>(mainMenuCanvas.at(2).get())),
+        std::shared_ptr<Button>(static_cast<Button*>(mainMenuCanvas.at(3).get())),
+        std::shared_ptr<Button>(static_cast<Button*>(mainMenuCanvas.at(4).get())),
+        std::shared_ptr<Button>(static_cast<Button*>(mainMenuCanvas.at(8).get()))
     };
 
     // GAME
@@ -76,27 +62,39 @@ int main() {
         if (!event.empty) {
             if (mainMenu) {
                 // Poll buttons and clickables
-                // (This is a kind of messy way of doing these pop-ups, I don't want to over-engineer it.)
-                for (Button* b : mainMenuClickables) {
-                    bool release = (*b).poll(std::shared_ptr<ClickEvent>(&event));
-                    if (release && b == &play) {
-                        Game::instance().init();
-                        mainMenu = false;
-                    } else if (release && b == &how) {
-                        howBackground.show();
-                        back.show();
-                    } else if (release && b == &stats) {
-                        statsBackground.show();
-                        back.show();
-                    } else if (release && b == &credits) {
-                        creditsBackground.show();
-                        back.show();
-                    } else if (release) { // Back button
-                        howBackground.hide();
-                        statsBackground.hide();
-                        creditsBackground.hide();
-                        back.hide();
+                // (This is a kind of messy way of doing these pop-ups, but I don't want to over-engineer it.)
+                int idx = 0;
+                for (auto&& button : mainMenuClickables) {
+                    bool release = button->poll(std::shared_ptr<ClickEvent>(&event));
+                    if (release) {
+                        switch (idx) {
+                            case 0: // PLAY
+                                mainMenu = false;
+                                Game::instance().init();
+                                break;
+                            case 1: // HOW TO PLAY
+                                mainMenuCanvas.at(5)->show();
+                                mainMenuCanvas.at(8)->show();
+                                break;
+                            case 2: // STATS
+                                mainMenuCanvas.at(6)->show();
+                                mainMenuCanvas.at(8)->show();
+                                break;
+                            case 3: // CREDITS
+                                mainMenuCanvas.at(7)->show();
+                                mainMenuCanvas.at(8)->show();
+                                break;
+                            case 4: // BACK
+                                mainMenuCanvas.at(5)->hide();
+                                mainMenuCanvas.at(6)->hide();
+                                mainMenuCanvas.at(7)->hide();
+                                mainMenuCanvas.at(8)->hide();
+                                break;
+                            default:
+                                std::cout << "Button registry error!";
+                        }
                     }
+                    idx++;
                 }
             } else {
                 // Poll buttons and clickables
@@ -104,10 +102,10 @@ int main() {
         }
 
         if (mainMenu) {
-            for (CanvasItem* c : mainMenuCanvas) {
-                (*c).draw();
+            for (auto&& item : mainMenuCanvas) {
+                item->draw();
             }
-            if (statsBackground.isVisible()) {
+            if (mainMenuCanvas.at(6)->isVisible()) {
                 // Writing twice for word shadow
                 LCD.SetFontColor(BLACK);
                 LCD.WriteAt(stringifyTime(longestTime),131,70);
